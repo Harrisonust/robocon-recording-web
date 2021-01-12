7a813ff544baf8f5f9b0701a36d3a0491885ae18
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2021-01-12 17:18:47 +0800

    temp

---
+++ src/Mapping/Map.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Map extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <button className="btn btn-primary ">I-type basket</button>
+      </div>
+    );
+  }
+}
+
+export default Map;
