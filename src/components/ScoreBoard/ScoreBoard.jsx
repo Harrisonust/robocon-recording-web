a8b251231e5d1336009876fe4eb3f1fe94d9f7f3
Harrison <sloab@ust.hk>
2021-01-23 01:25:11 +0800

    create class ScoreBoard

--- src/components/ScoreBoard/ScoreBoard.jsx
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //this is the parent class for Panel and GameField
 class ScoreBoard extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  state = { points: 0 };
+  render() {
+    return <div></div>;
+  }
 }
- 
+
 export default ScoreBoard;
